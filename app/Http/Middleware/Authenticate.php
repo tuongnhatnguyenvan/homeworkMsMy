4c521f105570ac63176d47d58a604facfe2913ae
tuongnhatnguyenvan <tuongnhatnv@gamail.com>
2024-04-03 21:06:22 +0700

    finish feature/single-device-login

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class Authenticate extends Middleware
 {
@@ -13,5 +14,38 @@ class Authenticate extends Middleware
     protected function redirectTo(Request $request): ?string
     {
         return $request->expectsJson() ? null : route('login');
+    }
+
+    protected function authenticate($request, array $guards)
+    {
+        if (empty($guards)) {
+            $guards = [null];
+        }
+
+        foreach ($guards as $guard) {
+            if ($this->auth->guard($guard)->check()) {
+
+                $checkDevice = $this->checkDevice($request);
+                if (!$checkDevice) {
+                    $this->unauthenticated($request, $guards);
+                }
+                return $this->auth->shouldUse($guard);
+            }
+        }
+
+        $this->unauthenticated($request, $guards);
+    }
+
+    private function checkDevice($request)
+    {
+        $sessionId = $request->session()->getId();
+        $user = $request->user();
+        $lastSessionId = $user->last_session;
+
+        if ($lastSessionId != $sessionId) {
+            Auth::logout();
+            return false;
+        }
+        return true;
     }
 }
