34967276b84ec3f19e0309008330fb01656055c1
tuongnhatnguyenvan <tuongnhatnv@gamail.com>
2024-04-09 23:14:11 +0700

    finish

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,6 +6,7 @@ use App\Models\Users;
 use Illuminate\Http\Request;
 use App\Http\Requests\UpdateUsersRequest;
 use OpenApi\Annotations as OA;
+use Illuminate\Support\Facades\Validator;
 
 class UsersController extends Controller
 {
@@ -63,6 +64,27 @@ class UsersController extends Controller
      */
     public function store(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|min:3|max:15',
+            'email' => 'required|string|email|unique:users,email',
+            'password' => 'required|string|confirmed',
+        ], [
+            'name.required' => 'Họ và tên bắt buộc phải nhập',
+            'name.string' => 'Họ và tên bắt buộc là string',
+            'name.min' => 'Họ và tên phải từ :min ký tự trở lên',
+            'name.max' => 'Họ và tên phải nhỏ hơn :max ký tự',
+            'email.required' => 'Email bắt buộc phải nhập',
+            'email.email' => 'Email không đúng định dạng',
+            'email.unique' => 'Email đã tồn tại trên hệ thống',
+            'email.string' => 'Email bắt buộc là string',
+            'password.required' => 'Password bắt buộc phải nhập',
+            'password.string' => 'Password bắt buộc là string',
+            'password.confirmed' => 'Password xác nhận không đúng',
+        ]);
+        if ($validator->fails()) {
+            $errors = $validator->errors()->all();
+            return response()->json($errors, 412);
+        }
         $data = [
             'name' => $request->input('name'),
             'email' => $request->input('email'),
@@ -126,6 +148,29 @@ class UsersController extends Controller
      */
     public function update(Request $request, $id)
     {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|min:3|max:15',
+            'email' => 'required|string|email|unique:users,email',
+            'password' => 'required|string|confirmed',
+        ], [
+            'name.required' => 'Họ và tên bắt buộc phải nhập',
+            'name.string' => 'Họ và tên bắt buộc là string',
+            'name.min' => 'Họ và tên phải từ :min ký tự trở lên',
+            'name.max' => 'Họ và tên phải nhỏ hơn :max ký tự',
+            'email.required' => 'Email bắt buộc phải nhập',
+            'email.email' => 'Email không đúng định dạng',
+            'email.unique' => 'Email đã tồn tại trên hệ thống',
+            'email.string' => 'Email bắt buộc là string',
+            'password.required' => 'Password bắt buộc phải nhập',
+            'password.string' => 'Password bắt buộc là string',
+            'password.confirmed' => 'Password xác nhận không đúng',
+        ]);
+
+        if ($validator->fails()) {
+            $errors = $validator->errors()->all();
+            return response()->json($errors, 412);
+        }
         $newData = [
             'name' => $request->input('name'),
             'email' => $request->input('email'),
